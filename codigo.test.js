b438b2b6c23e86f1c1de53eeb7372e0fb31a0a79
Daniella Maleski <daniella.maleski@gmail.com>
2025-02-17 20:21:35 -0300

    aula sobre o tdd com jest

---
+++ codigo.test.js
@@ -0,0 +1,32 @@
+const { soma, dobro } = require('./codigo')
+
+describe('Funções matemáticas', () =>{
+
+    beforeAll(() => {
+        console.log('antes de tudo')
+    })
+
+    beforeEach(() => {
+        console.log('antes de cada teste')
+    })
+
+    it('soma de dois valores', () =>{
+        expect(soma(2,5)).toBe(7)
+        expect(soma(2,4)).toBe(6)
+        expect(soma(21,44)).toBe(65)
+    }) 
+
+    it('dobro de um valor', () => {
+        expect(dobro(4)).toBe(8)
+    })
+
+    afterEach(() => {
+        console.log('depois de cada teste')
+    })
+
+    afterAll(() => {
+        console.log('depois de tudo')
+    })
+}) 
+
+
